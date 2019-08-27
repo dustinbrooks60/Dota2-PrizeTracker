ae6fa0488ac9d5a7d6a391919935a1070ceff6e8
dustinbrooks60 <dbrooks49@my.bcit.ca>
2019-08-27 11:16:27 -0700

    Adjusted pie chart on each page

--- wwwroot/js/demo/ti5_pie_chart.js
+++ wwwroot/js/demo/ti5_pie_chart.js
@@ -32,5 +32,31 @@ var myPieGraph = new Chart(ctx, {
       position: 'bottom'
     },
 
+
+  tooltips: {
+      callbacks: {
+        // this callback is used to create the tooltip label
+        label: function(tooltipItem, data) {
+          // get the data label and data value to display
+          // convert the data value to local string so it uses a comma seperated number
+          var dataLabel = data.labels[tooltipItem.index];
+          var value = ': ' + '$' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+
+          // make this isn't a multi-line label (e.g. [["label 1 - line 1, "line 2, ], [etc...]])
+          if (Chart.helpers.isArray(dataLabel)) {
+            // show value on first line of multiline label
+            // need to clone because we are changing the value
+            dataLabel = dataLabel.slice();
+            dataLabel[0] += value;
+          } else {
+            dataLabel += value;
+          }
+
+          // return the text to display on the tooltip
+          return dataLabel;
+        }
+      }
+    }
+
   },
 });
