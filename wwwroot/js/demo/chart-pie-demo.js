f4490f2c476dae4190573e7577085585f0997554
dustinbrooks60 <dbrooks49@my.bcit.ca>
2019-08-27 21:53:33 -0700

    Adjusted home page to include pie distribution

--- wwwroot/js/demo/chart-pie-demo.js
+++ wwwroot/js/demo/chart-pie-demo.js
@@ -3,15 +3,15 @@ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,Bli
 Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
+var ctx = document.getElementById("TI9PieChart");
+var myPieGraph = new Chart(ctx, {
+  type: 'pie',
   data: {
-    labels: ["Crowdfunded", "Initial"],
+    labels: ["1st Place", "2nd Place", "3rd Place", "4th Place", "5th-6th Place", "7th-8th Place", "9th-12th Place", "13th-16th Place", "17th-18th Place"],
     datasets: [{
-      data: [95, 5],
-      backgroundColor: ['#4e73df', '#1cc88a'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673'],
+      data: [15618808, 4462516, 3089435, 2059623, 1201447, 858177, 686541, 514906, 85818],
+      backgroundColor: ['#FFD700', '#C0C0C0', '#B08D57', "#b87333", "#B22222", "#8E44AD", "#4e73df", "#1cc88a", '#FFA500'],
+      hoverBackgroundColor: [],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
@@ -28,8 +28,35 @@ var myPieChart = new Chart(ctx, {
       caretPadding: 10,
     },
     legend: {
-      display: false
+      display: true,
+      position: 'bottom'
     },
-    cutoutPercentage: 80,
+
+
+  tooltips: {
+      callbacks: {
+        // this callback is used to create the tooltip label
+        label: function(tooltipItem, data) {
+          // get the data label and data value to display
+          // convert the data value to local string so it uses a comma seperated number
+          var dataLabel = data.labels[tooltipItem.index];
+          var value = ': ' + '$' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+
+          // make this isn't a multi-line label (e.g. [["label 1 - line 1, "line 2, ], [etc...]])
+          if (Chart.helpers.isArray(dataLabel)) {
+            // show value on first line of multiline label
+            // need to clone because we are changing the value
+            dataLabel = dataLabel.slice();
+            dataLabel[0] += value;
+          } else {
+            dataLabel += value;
+          }
+
+          // return the text to display on the tooltip
+          return dataLabel;
+        }
+      }
+    }
+
   },
 });
